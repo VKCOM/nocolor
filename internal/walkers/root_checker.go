ad0e4fd1d83b681c4d27fb8a72ef5bbc30a3fae8
i582 <i58215@voenmeh.ru>
2021-06-29 10:23:55 +0300

    internal/walkers: refactored and simplified code

--- internal/walkers/root_checker.go
+++ internal/walkers/root_checker.go
@@ -77,27 +77,19 @@ func (r *RootChecker) AfterEnterNode(n ir.Node) {
 		r.handleStaticCall(n, nil)
 	case *ir.MethodCallExpr:
 		r.handleMethodCall(n, nil, r)
-
-	case *ir.ClassStmt:
-		r.handleClassStmt(n.ClassName, n.Doc)
-	case *ir.InterfaceStmt:
-		r.handleClassStmt(n.InterfaceName, n.Doc)
-	case *ir.TraitStmt:
-		r.handleClassStmt(n.TraitName, n.Doc)
-
-	case *ir.ClassMethodStmt:
-		r.handleClassMethodStmt(n)
-	case *ir.FunctionStmt:
-		r.handleFunctionStmt(n)
 	case *ir.ImportExpr:
 		r.handleImportExpr(n)
-	}
-}
 
-func (r *RootChecker) handleClassStmt(className ir.Node, doc phpdoc.Comment) {
-	errs := r.checkPhpDocColors(doc)
-	for _, err := range errs {
-		r.ctx.Report(className, linter.LevelError, "errorColor", err)
+	case *ir.ClassStmt:
+		r.checkColorsInDoc(n.ClassName, n.Doc)
+	case *ir.InterfaceStmt:
+		r.checkColorsInDoc(n.InterfaceName, n.Doc)
+	case *ir.TraitStmt:
+		r.checkColorsInDoc(n.TraitName, n.Doc)
+	case *ir.ClassMethodStmt:
+		r.checkColorsInDoc(n.MethodName, n.Doc)
+	case *ir.FunctionStmt:
+		r.checkColorsInDoc(n.FunctionName, n.Doc)
 	}
 }
 
@@ -117,81 +109,12 @@ func (r *RootChecker) handleImportExpr(n *ir.ImportExpr) {
 		return
 	}
 
-	curFunc, ok := r.getCurrentFunc()
-	if !ok {
-		return
-	}
-
 	fileFunc, ok := r.globalCtx.Functions.Get(generateFileFuncName(path))
 	if !ok {
 		return
 	}
 
-	curFunc.Called.Add(fileFunc)
-	fileFunc.CalledBy.Add(curFunc)
-}
-
-func (r *RootChecker) getImportAbsPath(path string) (string, bool) {
-	if filepath.IsAbs(path) {
-		return filepath.Clean(path), true
-	}
-
-	// If relative path.
-	if strings.HasPrefix(path, ".") || strings.HasPrefix(path, "..") {
-		currentFilePath := r.ctx.Filename()
-		dir := filepath.Dir(currentFilePath)
-
-		absPath := filepath.Clean(filepath.Join(dir, path))
-		return absPath, true
-	}
-
-	return "", false
-}
-
-func (r *RootChecker) handleFunctionStmt(n *ir.FunctionStmt) {
-	errs := r.checkPhpDocColors(n.Doc)
-	for _, err := range errs {
-		r.ctx.Report(n.FunctionName, linter.LevelError, "errorColor", err)
-	}
-}
-
-func (r *RootChecker) handleClassMethodStmt(n *ir.ClassMethodStmt) {
-	errs := r.checkPhpDocColors(n.Doc)
-	for _, err := range errs {
-		r.ctx.Report(n.MethodName, linter.LevelError, "errorColor", err)
-	}
-}
-
-func (r *RootChecker) checkPhpDocColors(comment phpdoc.Comment) (errs []string) {
-	for _, part := range comment.Parsed {
-		p, ok := part.(*phpdoc.RawCommentPart)
-		if !ok {
-			continue
-		}
-
-		if p.Name() != r.colorTag {
-			continue
-		}
-
-		if len(p.Params) == 0 {
-			errs = append(errs, fmt.Sprintf("An empty '@%s' tag value", p.Name()))
-			continue
-		}
-
-		colorName := p.Params[0]
-
-		if colorName == "transparent" {
-			errs = append(errs, "Use of the 'transparent' color does not make sense")
-			continue
-		}
-
-		if !r.palette.ColorExists(colorName) {
-			errs = append(errs, fmt.Sprintf("Color '%s' missing in palette (either a misprint or a new color that needs to be added)", colorName))
-			continue
-		}
-	}
-
-	return errs
+	r.createEdgeToCurrent(fileFunc)
 }
 
 func (r *RootChecker) handleFunctionCall(n *ir.FunctionCallExpr, v ir.Visitor) {
@@ -209,13 +132,7 @@ func (r *RootChecker) handleFunctionCall(n *ir.FunctionCallExpr, v ir.Visitor) {
 		return
 	}
 
-	curFunc, ok := r.getCurrentFunc()
-	if !ok {
-		return
-	}
-
-	curFunc.Called.Add(calledFunc)
-	calledFunc.CalledBy.Add(curFunc)
+	r.createEdgeToCurrent(calledFunc)
 }
 
 func (r *RootChecker) handleStaticCall(n *ir.StaticCallExpr, blockScope *meta.Scope) {
@@ -312,13 +229,63 @@ func (r *RootChecker) handleMethod(name string, classType types.Map) {
 		}
 	}
 
-	curFunc, ok := r.getCurrentFunc()
-	if !ok {
-		return
+	r.createEdgeToCurrent(calledFunc)
+}
+
+func (r *RootChecker) checkColorsInDoc(name ir.Node, doc phpdoc.Comment) {
+	errs := r.getPhpDocColorErrors(doc)
+	for _, err := range errs {
+		r.ctx.Report(name, linter.LevelError, "errorColor", err)
+	}
+}
+
+func (r *RootChecker) getPhpDocColorErrors(comment phpdoc.Comment) (errs []string) {
+	for _, part := range comment.Parsed {
+		p, ok := part.(*phpdoc.RawCommentPart)
+		if !ok {
+			continue
+		}
+
+		if p.Name() != r.colorTag {
+			continue
+		}
+
+		if len(p.Params) == 0 {
+			errs = append(errs, fmt.Sprintf("An empty '@%s' tag value", p.Name()))
+			continue
+		}
+
+		colorName := p.Params[0]
+
+		if colorName == "transparent" {
+			errs = append(errs, "Use of the 'transparent' color does not make sense")
+			continue
+		}
+
+		if !r.palette.ColorExists(colorName) {
+			errs = append(errs, fmt.Sprintf("Color '%s' missing in palette (either a misprint or a new color that needs to be added)", colorName))
+			continue
+		}
 	}
 
-	curFunc.Called.Add(calledFunc)
-	calledFunc.CalledBy.Add(curFunc)
+	return errs
+}
+
+func (r *RootChecker) getImportAbsPath(path string) (string, bool) {
+	if filepath.IsAbs(path) {
+		return filepath.Clean(path), true
+	}
+
+	// If relative path.
+	if strings.HasPrefix(path, ".") || strings.HasPrefix(path, "..") {
+		currentFilePath := r.ctx.Filename()
+		dir := filepath.Dir(currentFilePath)
+
+		absPath := filepath.Clean(filepath.Join(dir, path))
+		return absPath, true
+	}
+
+	return "", false
 }
 
 func (r *RootChecker) getCurrentFunc() (*symbols.Function, bool) {
@@ -354,4 +321,14 @@ func (r *RootChecker) getCurrentFunc() (*symbols.Function, bool) {
 	}
 
 	return fn, true
+}
+
+func (r *RootChecker) createEdgeToCurrent(calledFunc *symbols.Function) {
+	curFunc, ok := r.getCurrentFunc()
+	if !ok {
+		return
+	}
+
+	curFunc.Called.Add(calledFunc)
+	calledFunc.CalledBy.Add(curFunc)
 }
